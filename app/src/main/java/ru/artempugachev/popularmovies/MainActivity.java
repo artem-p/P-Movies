2fe95331f44ec1dcf9a2e6a7c620542166604d13
artem-p <artempugachev1@gmail.com>
2017-07-10 15:39:09 +0300

    spinner in menu, looks bad

--- app/src/main/java/ru/artempugachev/popularmovies/MainActivity.java
+++ app/src/main/java/ru/artempugachev/popularmovies/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.List;
 
@@ -43,6 +46,14 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem spinnerItem = menu.findItem(R.id.sortOrderSpinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(spinnerItem);
+
+        String[] sortOrders = {getString(R.string.popular_label), getString(R.string.top_rated_label)};
+        ArrayAdapter sortOrderSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, sortOrders);
+        sortOrderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(sortOrderSpinnerAdapter);
         return true;
     }
 
