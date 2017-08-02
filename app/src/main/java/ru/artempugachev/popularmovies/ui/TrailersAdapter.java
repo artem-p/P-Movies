16ea7042076c9c985d0a4726386bb6d8f755e950
artem-p <artempugachev1@gmail.com>
2017-08-02 11:54:10 +0300

    add trailer item

--- app/src/main/java/ru/artempugachev/popularmovies/ui/TrailersAdapter.java
+++ app/src/main/java/ru/artempugachev/popularmovies/ui/TrailersAdapter.java
@@ -1,8 +1,39 @@
 package ru.artempugachev.popularmovies.ui;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import ru.artempugachev.popularmovies.data.Video;
+
 /**
  * Adapter for trailers grid
  */
 
-public class TrailersAdapter {
+public class TrailersAdapter extends RecyclerView.Adapter<TrailersAdapter.TrailerViewHolder> {
+    private List<Video> mTrailers;
+
+    @Override
+    public TrailerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(TrailerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTrailers != null ? mTrailers.size() : 0;
+    }
+
+    class TrailerViewHolder extends RecyclerView.ViewHolder {
+        public TrailerViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
 }
