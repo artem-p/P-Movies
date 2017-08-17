4962ac85471c66a413b26a6aa62dd6a71d5bd006
artem-p <artempugachev1@gmail.com>
2017-08-17 16:27:43 +0300

    favorites loader stub

--- app/src/main/java/ru/artempugachev/popularmovies/data/FavoritesLoader.java
+++ app/src/main/java/ru/artempugachev/popularmovies/data/FavoritesLoader.java
@@ -1,6 +1,10 @@
 package ru.artempugachev.popularmovies.data;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.util.List;
 
 /**
  * Load movies from local collection in db
@@ -14,6 +18,28 @@ public class FavoritesLoader extends MoviesLoader {
     @Override
     protected void onForceLoad() {
         moviesLoadListener.onStartLoadingMovies();
+
+        AsyncTask<Void, Void, List<Movie>> moviesLoadTask = new AsyncTask<Void, Void, List<Movie>>() {
+            @Override
+            protected List<Movie> doInBackground(Void... params) {
+                Toast.makeText(getContext(), "Start load movies from db", Toast.LENGTH_SHORT).show();
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List<Movie> movies) {
+                moviesLoadListener.onFinishLoadingMovies();
+                deliverResult(movies);
+            }
+
+            @Override
+            protected void onCancelled() {
+                moviesLoadListener.onFinishLoadingMovies();
+                super.onCancelled();
+            }
+        };
+
+        moviesLoadTask.execute();
 
         super.onForceLoad();
     }
