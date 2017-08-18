d9211579421ab557aa752d9610cabc8df286898c
artem-p <artempugachev1@gmail.com>
2017-08-18 17:48:10 +0300

    bigin

--- app/src/main/java/ru/artempugachev/popularmovies/MoviesGridLoader.java
+++ app/src/main/java/ru/artempugachev/popularmovies/MoviesGridLoader.java
@@ -15,7 +15,7 @@ import ru.artempugachev.popularmovies.tmdb.TmdbApiClient;
 import ru.artempugachev.popularmovies.tmdb.TmdbApiInterface;
 
 /**
- * We use loader to fetch tmdb data and load it to activity
+ * Fetch data from tmdb or local db and load it to activity
  */
 
 public class MoviesGridLoader extends Loader<List<Movie>> {
@@ -25,16 +25,6 @@ public class MoviesGridLoader extends Loader<List<Movie>> {
     }
 
 
-    /**
-     * Stores away the application context associated with context.
-     * Since Loaders can be used across multiple activities it's dangerous to
-     * store the context directly; always use {@link #getContext()} to retrieve
-     * the Loader's Context, don't use the constructor argument directly.
-     * The Context returned by {@link #getContext} is safe to use across
-     * Activity instances.
-     *
-     * @param context used to retrieve the application context.
-     */
     public MoviesGridLoader(Context context, MoviesLoadListener moviesLoadListener, int pageNumber) {
         super(context);
         this.moviesLoadListener = moviesLoadListener;
