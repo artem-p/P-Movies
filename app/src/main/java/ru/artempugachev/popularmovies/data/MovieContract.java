cf5944edeff6e90f571ddc196d18d0ede230a409
Artem Pugachev <artempugachev1@gmail.com>
2017-08-21 23:49:46 +0300

    Remove schematic dependency. Movie contract with create table sql

--- app/src/main/java/ru/artempugachev/popularmovies/data/MovieContract.java
+++ app/src/main/java/ru/artempugachev/popularmovies/data/MovieContract.java
@@ -1,16 +1,7 @@
 package ru.artempugachev.popularmovies.data;
 
-import android.content.ContentValues;
-import android.net.Uri;
+import android.provider.BaseColumns;
 
-import net.simonvt.schematic.annotation.AutoIncrement;
-import net.simonvt.schematic.annotation.DataType;
-import net.simonvt.schematic.annotation.NotNull;
-import net.simonvt.schematic.annotation.PrimaryKey;
-
-import static net.simonvt.schematic.annotation.DataType.Type.INTEGER;
-import static net.simonvt.schematic.annotation.DataType.Type.REAL;
-import static net.simonvt.schematic.annotation.DataType.Type.TEXT;
 
 /**
  * Contract for storing movies in db
@@ -21,38 +12,27 @@ public final class MovieContract {
 
     }
 
-    public interface MovieEntry {
-        @DataType(INTEGER)
-        @PrimaryKey
-        @AutoIncrement
-        String _ID = "_id";
+    private static final String TEXT_NOT_NULL = " TEXT NOT NULL, ";
 
-        @DataType(TEXT)
-        @NotNull
-        String TITLE = "title";
-
-        @DataType(INTEGER)
-        @NotNull
-        String RELEASE_DATE = "release_date";
-
-        @DataType(TEXT)
-        @NotNull
-        String MOVIE_ID = "movie_id";
-
-        @DataType(TEXT)
-        @NotNull
-        String POSTER_PATH = "poster_path";
-
-        @DataType(TEXT)
-        @NotNull
-        String OVERVIEW = "overview";
-
-        @DataType(TEXT)
-        @NotNull
-        String BACKDROP_PATH = "backdrop_path";
-
-        @DataType(REAL)
-        @NotNull
-        String VOTE_AVERAGE = "vote_average";
+    public static final class MovieEntry implements BaseColumns {
+        private static final String TABLE_NAME = "movies";
+        public static final String _ID = "_id";
+        public static final String TITLE = "title";
+        public static final String RELEASE_DATE = "release_date";
+        public static final String MOVIE_ID = "movie_id";
+        public static final String POSTER_PATH = "poster_path";
+        public static final String OVERVIEW = "overview";
+        public static final String BACKDROP_PATH = "backdrop_path";
+        public static final String VOTE_AVERAGE = "vote_average";
     }
+
+    public static final String CREATE_TABLE_SQL = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+            MovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            MovieEntry.MOVIE_ID + TEXT_NOT_NULL +
+            MovieEntry.TITLE + TEXT_NOT_NULL +
+            MovieEntry.RELEASE_DATE + TEXT_NOT_NULL +
+            MovieEntry.POSTER_PATH + TEXT_NOT_NULL +
+            MovieEntry.OVERVIEW + TEXT_NOT_NULL +
+            MovieEntry.BACKDROP_PATH + TEXT_NOT_NULL +
+            MovieEntry.VOTE_AVERAGE + " REAL NOT NULL" + ");";
 }
