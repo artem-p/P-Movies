5752edcc6e763554548d85e233da408f0688e4b3
artem-p <artempugachev1@gmail.com>
2018-03-25 18:56:55 +0300

    video to kotlin

---
+++ app/src/main/java/ru/artempugachev/popularmovies/model/Video.kt
@@ -0,0 +1,47 @@
+package ru.artempugachev.popularmovies.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Object to store video information from TMDB json response
+ */
+
+class Video {
+    @SerializedName("id")
+    private val id: String? = null
+
+    @SerializedName("name")
+    val name: String? = null
+
+    @SerializedName("type")
+    private val type: String? = null
+
+    @SerializedName("key")
+    private val youtubeId: String? = null
+    
+    private val THUMBNAIL_URL_FORMAT = "http://img.youtube.com/vi/%s/mqdefault.jpg"
+    private val TRAILER_URL_FORMAT = "https://www.youtube.com/watch?v=%s"
+
+
+    /**
+     * Check if the video is trailer
+     */
+    fun isTrailer(): Boolean {
+        return type == TRAILER_TAG
+    }
+
+
+    /**
+     * Get url for trailer thumbnail
+     */
+    val thumbnailUrl: String
+        get() = String.format(THUMBNAIL_URL_FORMAT, youtubeId)
+
+    val videoUrl: String
+        get() = String.format(TRAILER_URL_FORMAT, youtubeId)
+
+
+    companion object {
+        private val TRAILER_TAG = "Trailer"
+    }
+}
