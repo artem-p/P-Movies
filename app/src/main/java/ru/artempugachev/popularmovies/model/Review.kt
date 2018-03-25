f08fb1deb06d97ed7e2f7560d2f9e513e1392b40
artem-p <artempugachev1@gmail.com>
2018-03-25 18:58:40 +0300

    review to kotlin

---
+++ app/src/main/java/ru/artempugachev/popularmovies/model/Review.kt
@@ -0,0 +1,21 @@
+package ru.artempugachev.popularmovies.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Data class for reviews
+ */
+
+class Review (
+    @SerializedName("id")
+    val id: String,
+
+    @SerializedName("author")
+    val author: String,
+
+    @SerializedName("content")
+    val content: String,
+
+    @SerializedName("url")
+    val url: String
+)
