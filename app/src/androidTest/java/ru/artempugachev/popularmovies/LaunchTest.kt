c212f296c64d96617fc6d3e16a827c21653ef471
artem-p <artempugachev1@gmail.com>
2018-03-25 19:14:33 +0300

    add launch test

---
+++ app/src/androidTest/java/ru/artempugachev/popularmovies/LaunchTest.kt
@@ -0,0 +1,31 @@
+package ru.artempugachev.popularmovies
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Rule
+import ru.artempugachev.popularmovies.activity.MainActivity
+
+/**
+ * Launch app and check if views appear
+ */
+@RunWith(AndroidJUnit4::class)
+class LaunchTest {
+    @Rule
+    @JvmField
+    var activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun views_appear() {
+        onView(withId(R.id.main_activity_app_bar_layout)).check(matches(isDisplayed()))
+    }
+}
