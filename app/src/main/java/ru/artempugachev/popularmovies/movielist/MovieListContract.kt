7ec0857638929ede48418f3cb265f16be33c9fab
artem-p <artempugachev1@gmail.com>
2018-05-05 08:23:46 +0300

    add view in movie list contract

--- app/src/main/java/ru/artempugachev/popularmovies/movielist/MovieListContract.kt
+++ app/src/main/java/ru/artempugachev/popularmovies/movielist/MovieListContract.kt
@@ -1,2 +1,16 @@
 package ru.artempugachev.popularmovies.movielist
 
+/**
+ * Contract for movie list MVP
+ * */
+
+interface MovieListContract {
+    /**
+     * Define functions we exposed from the view layer
+     * */
+    interface View {
+        fun showMovies()
+        fun showProgress()
+        fun showMovieDetail()
+    }
+}
