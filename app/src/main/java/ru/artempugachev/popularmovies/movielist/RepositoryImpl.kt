b189e62426c1a6bba15d79a0890b05215c12979c
artem-p <artempugachev1@gmail.com>
2018-05-24 10:10:04 +0300

    add is up to date to repository

--- app/src/main/java/ru/artempugachev/popularmovies/movielist/RepositoryImpl.kt
+++ app/src/main/java/ru/artempugachev/popularmovies/movielist/RepositoryImpl.kt
@@ -5,6 +5,19 @@ import ru.artempugachev.popularmovies.movielist.api.Movie
 import ru.artempugachev.popularmovies.tmdb.TmdbApiInterface
 
 class RepositoryImpl(tmdbApiInterface: TmdbApiInterface): Repository {
+    private val lastUpdateTime = System.currentTimeMillis()
+    private val popularMovies = ArrayList<Movie>()
+    private val topRatedMovies = ArrayList<Movie>()
+
+
+    /**
+     * Check if cache is up to date
+     * */
+    private fun isUpToDate(): Boolean {
+        return System.currentTimeMillis() - lastUpdateTime < VALID_TIME
+    }
+
+
     override fun getPopularMoviesFromMemory(): Observable<List<Movie>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
@@ -27,5 +40,10 @@ class RepositoryImpl(tmdbApiInterface: TmdbApiInterface): Repository {
 
     override fun getTopRatedMovies(): Observable<List<Movie>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    companion object {
+        const val VALID_TIME = 60 * 1000    // time after cache expired
     }
 }
