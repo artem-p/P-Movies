b5d31775666a43dbef02e5b1b83011ffead1267b
artem-p <artempugachev1@gmail.com>
2018-05-29 12:16:54 +0300

    add movie details mvp contract

---
+++ app/src/main/java/ru/artempugachev/popularmovies/moviedetails/MovieDetailsMvpContract.kt
@@ -0,0 +1,40 @@
+package ru.artempugachev.popularmovies.moviedetails
+
+import android.content.Intent
+import ru.artempugachev.popularmovies.movielist.api.Movie
+import ru.artempugachev.popularmovies.movielist.api.Review
+import ru.artempugachev.popularmovies.movielist.api.Video
+
+
+/**
+ * In movie details we display information about single movie
+ * */
+interface MovieDetailsMvpContract {
+    interface View {
+        fun showDetails(movie: Movie)
+        fun showTrailers(trailers: List<Video>)
+        fun showReviews(reviews: List<Review>)
+
+        fun showTrailersError()
+        fun showReviewsError()
+    }
+
+
+    interface Presenter {
+        fun getMovieFromIntent(intent: Intent): Movie
+        fun loadTrailers(movie: Movie)
+        fun loadReviews(movie: Movie)
+
+        fun bindView(view: View)
+        fun insubscribeData()
+    }
+
+
+    /**
+     * Store movie in the model
+     * */
+    interface Model {
+        fun setMovie(movie: Movie)
+        fun getMovie(): Movie
+    }
+}
