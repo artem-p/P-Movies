608ccb5c58c5900a563e48305cddacd5faf888bb
artem-p <artempugachev1@gmail.com>
2018-05-31 11:56:40 +0300

    add component to the application

--- app/src/main/java/ru/artempugachev/popularmovies/MoviesApplication.kt
+++ app/src/main/java/ru/artempugachev/popularmovies/MoviesApplication.kt
@@ -6,6 +6,8 @@ import com.facebook.stetho.Stetho
 import ru.artempugachev.popularmovies.di.ContextModule
 import ru.artempugachev.popularmovies.di.DaggerMovieComponent
 import ru.artempugachev.popularmovies.di.MovieComponent
+import ru.artempugachev.popularmovies.moviedetails.di.DetailsComponent
+import ru.artempugachev.popularmovies.moviedetails.di.DetailsModule
 import ru.artempugachev.popularmovies.movielist.di.DaggerMovieListComponent
 import ru.artempugachev.popularmovies.movielist.di.MovieListComponent
 import ru.artempugachev.popularmovies.movielist.di.MovieListModule
@@ -15,6 +17,7 @@ import ru.artempugachev.popularmovies.tmdb.TmdbModule
 class MoviesApplication : Application() {
     private lateinit var movieComponent: MovieComponent
     private lateinit var movieListComponent: MovieListComponent
+    private lateinit var movieDetailsComponent: DetailsComponent
 
     override fun onCreate() {
         super.onCreate()
@@ -28,11 +31,21 @@ class MoviesApplication : Application() {
         movieComponent = DaggerMovieComponent.builder()
                 .contextModule(ContextModule(this))
                 .build()
+
+        movieDetailsComponent = DaggerDetailsComponent.builder()
+                .tmdbModule(TmdbModule(BuildConfig.TMDB_API_KEY))
+                .detailsModule(DetailsModule())
+                .build()
     }
 
 
     fun getMovieListComponent(): MovieListComponent {
         return movieListComponent
+    }
+
+
+    fun getMovieDetailsComponent(): DetailsComponent {
+        return movieDetailsComponent
     }
 
 
