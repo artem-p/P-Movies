851dff1513421f58913938febcb72d882c1fcb63
artem-p <artempugachev1@gmail.com>
2018-05-31 11:44:32 +0300

    implement details module

--- app/src/main/java/ru/artempugachev/popularmovies/moviedetails/di/DetailsModule.kt
+++ app/src/main/java/ru/artempugachev/popularmovies/moviedetails/di/DetailsModule.kt
@@ -2,18 +2,27 @@ package ru.artempugachev.popularmovies.moviedetails.di
 
 import dagger.Module
 import dagger.Provides
-import ru.artempugachev.popularmovies.moviedetails.MovieDetailsMvpContract
-import ru.artempugachev.popularmovies.moviedetails.MovieDetailsPresenterImpl
+import ru.artempugachev.popularmovies.moviedetails.*
+import ru.artempugachev.popularmovies.movielist.api.Movie
+import ru.artempugachev.popularmovies.tmdb.TmdbApiInterface
 
 
 @Module
 class DetailsModule {
-
     @Provides
     fun providePresenter(model: MovieDetailsMvpContract.Model): MovieDetailsMvpContract.Presenter {
         return MovieDetailsPresenterImpl(model)
     }
 
 
+    @Provides
+    fun provideModel(repository: DetailsRepository): MovieDetailsMvpContract.Model {
+        return MovieDetailsModelImplementation(repository)
+    }
 
+
+    @Provides
+    fun provideRepository(movie: Movie, tmdbApiInterface: TmdbApiInterface): DetailsRepositoryImpl {
+        return DetailsRepositoryImpl(movie, tmdbApiInterface)
+    }
 }
